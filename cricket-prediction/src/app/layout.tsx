94d08a4360c0ceaeb2eec272a80e027f7dbba048
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-09-06 16:20:38 +0530

    uopdate farcaster deploments

--- cricket-prediction/src/app/layout.tsx
+++ cricket-prediction/src/app/layout.tsx
@@ -9,10 +9,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+        <meta
+          name="fc:frame"
+          content='{
+      "version": "1",
+      "imageUrl": "https://media.tenor.com/X73qwZxReB0AAAAM/fiery-virat-kohli.gif",
+       "button": {
+          "title": "Cricket Mania",
+          "action": {
+            "type": "launch_frame",
+            "name": "Cricket Mania",
+            "url": "http://cricket-mania-lovat.vercel.app/",
+            "splashImageUrl": "https://media.tenor.com/yDA46Ztr58AAAAAM/virat-funny-virat-huh.gif",
+            "splashBackgroundColor": "black"
+          }
+        }
+     }'
+        />
+      </head>
+
       <body>
         <AppKitProvider>
           <>
-            <Navbar/>
+            <Navbar />
             {children}
           </>
         </AppKitProvider>
--- cricket-prediction/src/providers/client-providers.tsx
+++ cricket-prediction/src/providers/client-providers.tsx
@@ -4,7 +4,10 @@ import { WagmiProvider } from 'wagmi'
 import { monadTestnet } from '@reown/appkit/networks'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { sdk } from "@farcaster/frame-sdk";
+import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import toast, { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react'
 const projectId = '684cdccc0de232f65a62603583571f5e'
 // 2. Create a metadata object - optional
 const metadata = {
@@ -33,6 +36,29 @@ createAppKit({
 
 export default function AppKitProvider({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        // Only run inside Farcaster Mini App environment
+        if (sdk?.actions) {
+          sdk.actions.ready();
+          console.log("✅ Farcaster ready called");
+        } else {
+          console.warn("Not running inside Farcaster frame");
+        }
+        setIsReady(true);
+      } catch (error) {
+        console.error("❌ Failed to initialize Farcaster:", error);
+      }
+    };
+
+    // Call after first render
+    init();
+  }, []);
+
+  if (!isReady) return null; 
   return (
     <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
